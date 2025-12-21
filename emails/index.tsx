78f1dc5d4879e933b5e53322716143e40cce77be
Optify <optifyai.app@gmail.com>
2025-12-21 15:20:46 +1300

    Remove Discord link from email template (restore to X/Twitter only)

--- emails/index.tsx
+++ emails/index.tsx
@@ -40,17 +40,6 @@ export const NotionWaitlistEmail = ({ userFirstname }: EmailProps) => (
           <a href="https://x.com/optifyai" style={link}>
             @optifyai
           </a>
-          {" or "}
-          <a href="https://discord.gg/GGKAVYYENQ" style={{ ...link, verticalAlign: 'middle', marginLeft: 4, display: 'inline-block' }}>
-            <img
-              src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/discord.svg"
-              alt="Discord"
-              width="18"
-              height="18"
-              style={{ display: 'inline', verticalAlign: 'middle', marginRight: 2 }}
-            />
-            Discord
-          </a>
         </Text>
         <Text style={signOff}>
           Best regards,
